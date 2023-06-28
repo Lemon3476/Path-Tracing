f8ac666cc957e925cdbd4d4d022c10de12785976
Zhenhui YU <yzh1349610764@gmail.com>
2023-06-28 17:25:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Author: Zhenhui YU
+
+## Features:
+•	Implemented path tracing and performed per-pixel shading based on the calculation of direct and indirect lighting.  
+•	Developed a sampling method and probability density function for the realistic rendering of diffuse materials.  
+•	Created a bounding box for each object to accelerate ray-object intersection calculations.  
+•	Divided the screen space into different parts and utilized multithreading on these parts to enhance rendering speed.  
+
+## Notes:
+I ran this code in the Linux system.  
+It Is implemented for learning purposes.  
+
+
---
+++ Renderer.cpp
@@ -0,0 +1,69 @@
+#include <fstream>
+#include "Scene.hpp"
+#include "Renderer.hpp"
+#include <mutex>
+#include<thread>
+ 
+std::mutex mtx;
+ 
+inline float deg2rad(const float& deg) { return deg * M_PI / 180.0; }
+ 
+const float EPSILON = 0.001;
+ 
+void Renderer::Render(const Scene& scene)
+{
+    std::vector<Vector3f> framebuffer(scene.width * scene.height);
+ 
+    float scale = tan(deg2rad(scene.fov * 0.5));
+    float imageAspectRatio = scene.width / (float)scene.height;
+    Vector3f eye_pos(278, 273, -800);
+ 
+    // change the spp value to change sample ammount
+    int spp = 16;
+    std::cout << "SPP: " << spp << "\n";
+    
+    int process = 0;
+    const int thred = 20;
+    int times = scene.height / thred;
+    std::thread th[thred];
+ 
+    auto castRayMultiThread = [&](uint32_t y_min, uint32_t y_max){
+        for (uint32_t j = y_min; j < y_max; j++) {
+            int m = j * scene.width;
+            for (uint32_t i = 0; i < scene.width; i++) {
+                float x = (2 * (i + 0.5) / (float)scene.width - 1) *
+                    imageAspectRatio * scale;
+                float y = (1 - 2 * (j + 0.5) / (float)scene.height) * scale;
+ 
+                Vector3f dir = normalize(Vector3f(-x, y, 1));
+                for (int k = 0; k < spp; k++) {
+                    framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;
+                }
+                m++;
+            }
+            mtx.lock();
+            process++;
+            UpdateProgress(1.0 * process / scene.height);
+            mtx.unlock();
+        }
+    };
+ 
+    for (int i = 0; i < thred; i++) {
+        th[i] = std::thread(castRayMultiThread, i * times, (i + 1) * times);
+    }
+    for (int i = 0; i < thred; i++) {
+        th[i].join();
+    }
+    UpdateProgress(1.f);
+ 
+    FILE* fp = fopen("binary.ppm", "wb");
+    (void)fprintf(fp, "P6\n%d %d\n255\n", scene.width, scene.height);
+    for (auto i = 0; i < scene.height * scene.width; ++i) {
+        static unsigned char color[3];
+        color[0] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].x), 0.6f));
+        color[1] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].y), 0.6f));
+        color[2] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].z), 0.6f));
+        fwrite(color, 1, 3, fp);
+    }
+    fclose(fp);
+}
